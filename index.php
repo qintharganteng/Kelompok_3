0c444955e0f0766d2bedc05d4fefa6617419c52a
unknown <qintharalmaliki@gmail.com>
2023-01-12 01:16:52 +0700

    sementara

--- index.php
+++ index.php
@@ -1,49 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parallax</title>
     <link rel="stylesheet" href="style.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> --> -->
 </head>
+
 <body>
     <!-- Membuat Navigator -->
-<nav>
-    <div class="wrapper">
-        <div class="logo"><a href="">PARALLAX</a></div>
-        <div class="menu">
-            <ul class="menu1">
-                <li><a href="#home">HOME</a></li>
-                <li><a href="login.php" class="tbl-blue">LOG IN</a></li>
-            <li> 
-                <div class="tombol">
-            <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button type="button" class="btn btn-outline-dark">Search</button>
-            </form></div>
-            </li>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href="">ABSENSI PARRALAX</a></div>
+            <div class="menu">
+                <ul class="menu1">
+                    <li><a href="#home">HOME</a></li>
+                    <li><a href="login.php" class="tbl-blue">LOG IN</a></li>
+                    <li><a href="1"></a></li>
+                    <li><a href="2"></a></li>
+                    <li>
+                        <div class="tombol">
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button type="button" class="btn btn-outline-dark">Search</button>
+                            </form>
+                        </div>
+                    </li>
 
-            </ul>
-          
+                </ul>
+
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
     <!-- Membuat Home -->
-<section id="home">\
-    <div class="welcome">
-        <h2>WELCOME</h2>
-    </div>
-</section>
+    <section id="home">
+    </section>
 
-<!-- LOG IN -->
-<!-- HOME -->
-<section id="home">\
-    <div class="login">
-        <h2></h2>
+    <!-- LOG IN -->
+    <!-- HOME -->
+    <section id="home">
+        <div class="login">
+            <h2>WELCOME</h2>
+        </div>
+    </section>
+
+
+     <section id="1">
+        <div class="pengertian">
+            <h2>Parallax</h2>
+            <div class="pengetiann">
+                <p>Parallax atau parallax website adalah sebuah fitur yang memiliki desain yang dapat memungkinkan tamplan sebuah website melakukan transisi saat pengunung sedang scrolling menggunakan mouse. Parallax website atau bisa disebut juga parallax scrolling merupakan fitur yang dapat menjadikan tampilan sebuat web menjadi hidup</p>
+            </div>
+        </div>
+    </section>
+    <br><br>
+    <section id="2">
+        <div class="kelebihan">
+            <center><h1>Kelebihan dan Kekurangan Parallax</h1></center>
+            <h2>Kelebihan Parallax</h2>
+            <p>Memberikan kedalaman dan efek animasi yang memukau pengunjung
+
+                <br>Membuat website dikunjungi lebih lama karena harus scroll terus ke bawah
+
+                <br>Meningkatkan kredibilitas website karena ada sesuatu yang baru dan kreatif
+            </p>
+            <h1>Kekurangan parallax</h1>
+            <p>Website yang hanya memiliki satu halaman biasanya hanya punya satu informasi meta.<br>
+
+                Tidak kompatibel dengan responsive dan mobile desain
+                <br>
+                Tidak ada halaman link internal dalam website tersebut karenya hanya satu halaman saja
+            </p>
+        </div>
+    </section> --> -->
+
+<!-- <div class="slide">
+    <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="foto.1.jpg" class="d-block w-100" alt="...">
     </div>
-</section>
-    
+    <div class="carousel-item">
+      <img src="foto.2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="foto.3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button> -->
+</div>
+</div>
 </body>
 </html>
+<!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script> -->
